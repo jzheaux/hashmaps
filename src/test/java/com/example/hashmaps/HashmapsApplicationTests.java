a8ee82752ea6b40218dd5522570e7f479aceced1
Josh Cummings <josh.cummings@gmail.com>
2023-06-13 09:15:44 -0600

    Using SessionRegistry

--- src/test/java/com/example/hashmaps/HashmapsApplicationTests.java
+++ src/test/java/com/example/hashmaps/HashmapsApplicationTests.java
@@ -2,59 +2,57 @@ package com.example.hashmaps;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.util.Assert;
 
-import java.util.Map;
 import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class HashmapsApplicationTests {
-    private final Map<CompositeKey, String> values = new ConcurrentHashMap<>();
+    private final SessionRegistry sessions = new SessionRegistryImpl();
+
+    //private final Map<CompositeKey, String> values = new ConcurrentHashMap<>();
 
     @Test
     void saveWithSubAndSidAndLoadWithSidOnly() {
-        CompositeKey key = new CompositeKey("sub1", "sid1"); // sign in passed both sub and sid
-        String value = "hello";
-        this.values.put(key, value); // saved to registry
-        String retrieved = this.values.get(new CompositeKey(null, "sid1")); // logout token only has sid
-        assertThat("hello").isSameAs(retrieved); // fails
+        Principal principal = new Principal("sub1", "sid1"); // sign in passed both sub and sid
+        this.sessions.registerNewSession("clientSid", principal);
+        Principal fromToken = new Principal(null, "sid1"); // logout token only has sid
+        assertThat(this.sessions.getAllSessions(fromToken, true)).isNotEmpty(); // fails
     }
 
     @Test
     void saveWithSubAndLoadWithSub() {
-        CompositeKey key = new CompositeKey("sub1", null); // sign in passed only sub
-        String value = "hello";
-        this.values.put(key, value); // saved to registry
-        String retrieved = this.values.get(new CompositeKey("sub1", null)); // logout token only has sub
-        assertThat("hello").isSameAs(retrieved); // succeeds
+        Principal principal = new Principal("sub1", null); // sign in passed both sub
+        this.sessions.registerNewSession("clientSid", principal);
+        Principal fromToken = new Principal("sub1", null); // logout token only has sub
+        assertThat(this.sessions.getAllSessions(fromToken, true)).isNotEmpty();
     }
 
     @Test
     void saveWithSubAndSidAndLoadWithSubAndSid() {
-        CompositeKey key = new CompositeKey("sub1", "sid1"); // sign in passed both sub and sid
-        String value = "hello";
-        this.values.put(key, value); // saved to registry
-        String retrieved = this.values.get(new CompositeKey("sub1", "sid1")); // logout token only has sub and sid
-        assertThat("hello").isSameAs(retrieved); // succeeds
+        Principal principal = new Principal("sub1", "sid1"); // sign in passed both
+        this.sessions.registerNewSession("clientSid", principal);
+        Principal fromToken = new Principal("sub1", "sid1"); // logout token has both
+        assertThat(this.sessions.getAllSessions(fromToken, true)).isNotEmpty();
     }
 
     @Test
     void saveWithSubAndSidAndLoadWithSubOnly() {
-        CompositeKey key = new CompositeKey("sub1", "sid1"); // sign in passed both sub and sid
-        String value = "hello";
-        this.values.put(key, value); // saved to registry
-        String retrieved = this.values.get(new CompositeKey("sub1", null)); // logout token only has sub
-        assertThat("hello").isSameAs(retrieved); // fails
+        Principal principal = new Principal("sub1", "sid1"); // sign in passed both
+        this.sessions.registerNewSession("clientSid", principal);
+        Principal fromToken = new Principal("sub1", null); // logout token only has sub
+        assertThat(this.sessions.getAllSessions(fromToken, true)).isNotEmpty(); // fails
     }
 
-    private static final class CompositeKey {
+    private static final class Principal {
         private final String subject;
         private final String sessionId;
 
-        public CompositeKey(String subject, String sessionId) {
+        public Principal(String subject, String sessionId) {
             Assert.isTrue(subject != null || sessionId != null, "either subject or sessionid must not be null");
             this.subject = subject;
             this.sessionId = sessionId;
@@ -72,7 +70,7 @@ class HashmapsApplicationTests {
             if (obj == this) {
                 return true;
             }
-            if (!(obj instanceof CompositeKey that)) {
+            if (!(obj instanceof Principal that)) {
                 return false;
             }
             return equalsIdeaOne(that);
@@ -84,14 +82,14 @@ class HashmapsApplicationTests {
             // return equalsIdeaTwo(that);
         }
 
-        private boolean equalsIdeaOne(CompositeKey that) {
+        private boolean equalsIdeaOne(Principal that) {
             if (that.sessionId == null) {
                 return this.subject.equals(that.subject);
             }
             return this.sessionId.equals(that.sessionId) && this.subject.equals(that.subject);
         }
 
-        private boolean equalsIdeaTwo(CompositeKey that) {
+        private boolean equalsIdeaTwo(Principal that) {
             return Objects.equals(that.sessionId, this.sessionId) && Objects.equals(that.subject, this.subject);
         }
 
